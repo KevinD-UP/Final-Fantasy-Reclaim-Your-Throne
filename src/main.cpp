f01adda140caf252a34ccbf1b3fe9841d684f542
Kévin DANG <kdang@pitchy.fr>
2021-12-31 23:46:40 +0100

    init partie en fonction du perso choisi

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,154 @@
 #include "header/model/Joueur/Joueur.h"
 #include "header/Partie.h"
 
-int main(){
+#include  <random>
+#include  <iterator>
 
-    PersonnageType TYPE_Guerrier = PT_Guerrier;
-    PersonnageType TYPE_Amazone = PT_Amazone;
-    PersonnageType TYPE_Moine = PT_Moine;
-    PersonnageType TYPE_Sorcier = PT_Sorcier;
+template<typename Iter, typename RandomGenerator>
+Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
+    std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+    std::advance(start, dis(g));
+    return start;
+}
+
+template<typename Iter>
+Iter select_randomly(Iter start, Iter end) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    return select_randomly(start, end, gen);
+}
+
+PersonnageType TYPE_Guerrier = PT_Guerrier;
+PersonnageType TYPE_Amazone = PT_Amazone;
+PersonnageType TYPE_Moine = PT_Moine;
+PersonnageType TYPE_Sorcier = PT_Sorcier;
+
+static vector<std::string> names = {
+        "Aien Xilcan",
+        "Gorwin Fenroris",
+        "Sinaht Zinnorin",
+        "Raeran Mirasatra",
+        "Volodar Petwraek",
+        "Khatar Lorabella",
+        "Fenian Naezumin",
+        "Hagmar Xyrneiros",
+        "Folduin Triszeiros",
+        "Alas Keaynore",
+        "Gaeleath Valrona",
+        "Elen Shastina",
+        "Cithrel Glynceran",
+        "Faelyn Helewarin",
+        "Khyrmin Jokian",
+        "Ilphas Gilmaer",
+        "Elauthin Adric",
+        "Eldaerenth Roceran",
+        "Artin Zylra",
+        "Nym Morcaryn",
+        "Ynaselle Waesdi",
+        "Ilyana Caihice",
+        "Nephinae Zumfaren",
+        "Liluth Herlee",
+        "Gwynnestri Qizana",
+        "Nabeora Caivalur",
+        "Saria Iarnelis",
+        "Tehlarissa Miraren",
+        "Shanyrria Yinkian",
+        "Daethie Xyrran",
+        "Aegnor",
+        "Amras",
+        "Amrod",
+        "Anairë",
+        "Angrod",
+        "Aranwë",
+        "Aredhel",
+        "Caranthir",
+        "Celebrimbor",
+        "Celegorm",
+        "Curufin",
+        "Ecthelion",
+        "Edrahil",
+        "Elemmakil",
+        "Elenwë",
+        "Enerdhil",
+        "Erestor",
+        "Fëanor",
+        "Finarfin",
+        "Findis",
+        "Finduilas",
+        "Fingolfin",
+        "Fingon",
+        "Finrod",
+        "Finwë",
+        "Galadriel",
+        "Gelmir",
+        "Gildor",
+        "Gil-galad",
+        "Glorfindel",
+        "Guilin",
+        "Gwindor",
+        "Idril",
+        "Maedhros",
+        "Maeglin",
+        "Maglor",
+        "Mahtan",
+        "Míriel",
+        "Nerdanel",
+        "Orodreth",
+        "Rúmil",
+        "Turgon",
+        "Voronwë",
+        "Aldor",
+        "Baldor",
+        "Brego",
+        "Brytta",
+        "Ceorl",
+        "Déor",
+        "Déorwine",
+        "Dúnhere",
+        "Elfhelm",
+        "Éofor",
+        "Éomer",
+        "Éomund",
+        "Eorl le Jeune",
+        "Éowyn",
+        "Erkenbrand",
+        "Fastred",
+        "Folca",
+        "Folcwine",
+        "Folcred",
+        "Fréa",
+        "Fréaláf",
+        "Fréawine",
+        "Freca",
+        "Gálmód",
+        "Gléowine",
+        "Goldwine",
+        "Gram",
+        "Gríma",
+        "Grimbold",
+        "Guthláf",
+        "Háma",
+        "Helm",
+        "Herefara",
+        "Herubrand",
+        "Horn",
+        "Thengel",
+        "Théoden",
+        "Théodred",
+        "Théodwyn",
+        "Walda",
+        "Wídfara"
+};
+
+int main(){
 
     std::string nomPerso;
     int choixPerso;
-    Joueur *joueur;
-    Personnage *personnageJoueur;
+    Joueur* joueur;
+    Personnage* personnageJoueur;
+    Personnage* Ennemie1;
+    Personnage* Ennemie2;
+    Personnage* Ennemie3;
 
     //TODO: INIT LES PERSO EN FONCTION DU JOUEUR
 
@@ -41,28 +178,39 @@ int main(){
         personnageJoueur = PersonnageFactory::initPersonnage(TYPE_Guerrier, nomPerso);
         joueur = new Joueur(personnageJoueur);
         std::cout << "Votre personnage a été crée" << std::endl;
+        std::cout << "Création des personnages non joueur..." << std::endl;
+        ennemie1 = PersonnageFactory::initPersonnage(TYPE_Sorcier,*select_randomly(names.begin(), names.end()));
+        ennemie2 = PersonnageFactory::initPersonnage(TYPE_Moine,*select_randomly(names.begin(), names.end()));
+        ennemie3 = PersonnageFactory::initPersonnage(TYPE_Amazone,*select_randomly(names.begin(), names.end()));
     }else if (choixPerso == 2){
         personnageJoueur = PersonnageFactory::initPersonnage(TYPE_Amazone, nomPerso);
         joueur = new Joueur(personnageJoueur);
         std::cout << "Votre personnage a été crée" << std::endl;
+        std::cout << "Création des personnages non joueur..." << std::endl;
+        ennemie1 = PersonnageFactory::initPersonnage(TYPE_Guerrier,*select_randomly(names.begin(), names.end()));
+        ennemie2 = PersonnageFactory::initPersonnage(TYPE_Moine,*select_randomly(names.begin(), names.end()));
+        ennemie3 = PersonnageFactory::initPersonnage(TYPE_Amazone,*select_randomly(names.begin(), names.end()));
     }else if (choixPerso == 3){
         personnageJoueur = PersonnageFactory::initPersonnage(TYPE_Moine, nomPerso);
         joueur = new Joueur(personnageJoueur);
         std::cout << "Votre personnage a été crée" << std::endl;
+        std::cout << "Création des personnages non joueur..." << std::endl;
+        ennemie1 = PersonnageFactory::initPersonnage(TYPE_Sorcier,*select_randomly(names.begin(), names.end()));
+        ennemie2 = PersonnageFactory::initPersonnage(TYPE_Guerrier,*select_randomly(names.begin(), names.end()));
+        ennemie3 = PersonnageFactory::initPersonnage(TYPE_Amazone,*select_randomly(names.begin(), names.end()));
     }else if(choixPerso == 4){
         personnageJoueur = PersonnageFactory::initPersonnage(TYPE_Sorcier, nomPerso);
         joueur = new Joueur(personnageJoueur);
         std::cout << "Votre personnage a été crée" << std::endl;
+        std::cout << "Création des personnages non joueur..." << std::endl;
+        ennemie1 = PersonnageFactory::initPersonnage(TYPE_Sorcier,*select_randomly(names.begin(), names.end()));
+        ennemie2 = PersonnageFactory::initPersonnage(TYPE_Moine,*select_randomly(names.begin(), names.end()));
+        ennemie3 = PersonnageFactory::initPersonnage(TYPE_Guerrier,*select_randomly(names.begin(), names.end()));
     }
-
-    std::cout << "Création des personnages non joueur..." << std::endl;
-    Personnage* sorcier = PersonnageFactory::initPersonnage(TYPE_Sorcier,"Test2");
-    Personnage* moine = PersonnageFactory::initPersonnage(TYPE_Moine,"Test3");
-    Personnage* amazone = PersonnageFactory::initPersonnage(TYPE_Amazone,"Test4");
 
     Map* carte = new Map();
 
-    Partie partie = Partie({personnageJoueur, sorcier, moine, amazone}, joueur , carte);
+    Partie partie = Partie({personnageJoueur, ennemie1, ennemie2, ennemie3}, joueur , carte);
     partie.startToPlay();
     //partie.deathBattle(personnageJoueur,sorcier);
     /*Piece *test = new Piece();
@@ -75,3 +223,4 @@ int main(){
 
    // std::cout << "Fin de la partie" << std::endl;
 }
+

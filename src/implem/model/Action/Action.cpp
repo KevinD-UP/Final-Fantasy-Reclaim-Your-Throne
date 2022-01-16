761e25ce8b7ecb65d65de468866e78d7650b6b31
Kévin DANG <kdang@pitchy.fr>
2022-01-16 23:43:39 +0100

    Cleaning for print

--- src/implem/model/Action/Action.cpp
+++ src/implem/model/Action/Action.cpp
@@ -9,17 +9,21 @@ Action::Action(Personnage* lanceurArg, Personnage* receveurArg, std::string nomA
 void Action::utilisation(){
     if(player->getPerso() == lanceur || player->getPerso() == receveur){
         if (type == Offensive) {
-            std::cout << "L'action " << nom << " a été utiliser sur " << receveur->getNom() << std::endl;
+            std::cout << "|--------------------------------------------------------------------------------------|" << std::endl;
+            std::cout << "| L'action " << nom << " a été utilisé sur " << receveur->getNom() << std::endl;
             if (dommage < 0) {
                 dommage = 0;
             }
             receveur->setSante(receveur->getSante() - dommage);
-            std::cout << receveur->getNom() << " recoit " << dommage << " de dommage" << std::endl << std::endl;
+            std::cout << "| " << receveur->getNom() << " recoit " << dommage << " de dommage" << "               |" << std::endl;
+            std::cout << "|--------------------------------------------------------------------------------------|" << std::endl << std::endl;
         }
         if (type == Defensive) {
-            std::cout << "L'action " << nom << " a été utiliser sur " << lanceur->getNom() << std::endl;
+            std::cout << "|--------------------------------------------------------------------------------------|" << std::endl;
+            std::cout << "| L'action " << nom << " a été utiliser sur " << lanceur->getNom() << std::endl;
             lanceur->setSante(lanceur->getSante() + dommage);
-            std::cout << lanceur->getNom() << " recoit " << dommage << " de soin" << std::endl << std::endl;
+            std::cout << "| " << lanceur->getNom() << " recoit " << dommage << " de soin                         |" << std::endl;
+            std::cout << "|--------------------------------------------------------------------------------------|" << std::endl << std::endl;
         }
     }
     else {
--- src/implem/model/Personnage/Loup.cpp
+++ src/implem/model/Personnage/Loup.cpp
@@ -36,19 +36,19 @@ void Loup::drop() {
     std::uniform_int_distribution<int> dist(0, 3);
     int drop_item = dist(engine);
     if (drop_item == 0){
-        auto* Armure_du_Loup = new Equipement("Armure_du_Loup", OT_Equipement, "Attaque:15 Defense:10 Sante:30", PT_Guerrier, false, 15, 10, 30, 2);
+        auto* Armure_du_Loup = new Equipement("Armure_du_Loup", OT_Equipement, "Attaque: 15 Defense: 10 Sante: 30", PT_Guerrier, false, 15, 10, 30, 2);
         pushSac(Armure_du_Loup);
     }
     if (drop_item == 1){
-        auto* Toge_du_Loup = new Equipement("Toge_du_Loup",OT_Equipement,"Attaque:10 Defense:10 Sante 50",PT_Moine,false,10, 10, 50, 2);
+        auto* Toge_du_Loup = new Equipement("Toge_du_Loup",OT_Equipement,"Attaque: 10 Defense: 10 Sante: 50",PT_Moine,false,10, 10, 50, 2);
         pushSac(Toge_du_Loup);
     }
     if (drop_item == 2){
-        auto* Arc_du_Loup = new Equipement("Arc_du_Loup",OT_Equipement,"Attaque:20 Deffense:5 Sante:30",PT_Amazone,false,20, 5, 30, 2);
+        auto* Arc_du_Loup = new Equipement("Arc_du_Loup",OT_Equipement,"Attaque: 20 Defense: 5 Sante: 30",PT_Amazone,false,20, 5, 30, 2);
         pushSac(Arc_du_Loup);
     }
     if (drop_item == 3){
-        auto* Robe_du_Loup = new Equipement("Robe_du_Loup",OT_Equipement,"Attaque:15 Defense:10 Sante:20",PT_Sorcier,false,5, 10, 15, 2);
+        auto* Robe_du_Loup = new Equipement("Robe_du_Loup",OT_Equipement,"Attaque: 15 Defense: 10 Sante: 20",PT_Sorcier,false,5, 10, 15, 2);
         pushSac(Robe_du_Loup);
     }
     getPieceCour()->pushObjet(sac[0]);
